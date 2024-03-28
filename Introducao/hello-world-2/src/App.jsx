0432cb133e8addb382ac12f72d9f7555dd57eb17
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-03-28 18:59:53 -0300

    Testando formatacao do Prettier

--- Introducao/hello-world-2/src/App.jsx
+++ Introducao/hello-world-2/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='text-slate-900 dark:text-white text-3xl font-bold p-8 '>Primeiro Projeto de React</h1>
-      <p className='text-cyan-500'>Páragrafo 1</p>
-      <p className='text-cyan-500'>Parágrafo 2</p>
-      <p className='text-cyan-500'>Parágrafo 3</p>
+      <h1 className="text-slate-900 dark:text-white text-3xl font-bold p-8 ">
+        Primeiro Projeto de React
+      </h1>
+      <p className="text-cyan-500">Páragrafo 1</p>
+      <p className="text-cyan-500">Parágrafo 2</p>
+      <p className="text-cyan-500">Parágrafo 3</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
