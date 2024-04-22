875af324be72b1e5c30defdddd04e5b22d587a40
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-04-22 17:58:21 -0300

    Desafio 3

---
+++ Avancando_em_Reacts/avancandoEmReact/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-normal text-cyan-500">Avançando em React</h1>
+    </>
+  );
+}
+
+export default App;
