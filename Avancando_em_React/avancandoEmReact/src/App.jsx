f2cf7306363d8951d668a4afab9f5c424900a35a
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-06 18:53:10 -0300

    Hooks

--- Avancando_em_React/avancandoEmReact/src/App.jsx
+++ Avancando_em_React/avancandoEmReact/src/App.jsx
@@ -1,17 +1,17 @@
-import Image from './assets/img1.jpg'
+import Image from "./assets/img1.jpg";
+import ManageData from "./components/ManageData";
 
 function App() {
   return (
     <>
-      <h1 className="p-8 text-5xl text-normal text-cyan-500">
+      <h1 className="p-8 text-5xl text-normal text-center text-cyan-500">
         Avançando em React
       </h1>
-      <div className="w-full">
-        <img src="/img2.jpg" alt="Relógio Steampunk" />
+      <div className="flex flex-col sm:flex-row justify-center">
+        <img className="w-72 mx-auto sm:mx-0" src="/img2.jpg" alt="Relógio Steampunk" />
+        <img className="w-72 mx-auto sm:mx-0" src={Image} alt="Paisagem Steampunk" />
       </div>
-      <div className="w-full">
-        <img src={Image} alt="Paisagem Steampunk" />
-      </div>
+      <ManageData />
     </>
   );
 }
---
+++ Avancando_em_React/avancandoEmReact/src/components/ManageData.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const ManageData = () => {
+  let someData = 10;
+	const [number, setNumber] = useState(10);
+  return (
+    <div className="p-12 gap-12 flex flex-col sm:flex-row justify-center text-center text-slate-900">
+			<div>
+				<p>Valor: {someData}</p>
+				<button className="text-normal text-slate-50 bg-cyan-500 hover:opacity-90 px-4 py-2 mt-4" onClick={() => {someData = 15}}>Mudar variável</button>
+			</div>
+			<div>
+				<p>Valor: {number}</p>
+				<button className="text-normal text-slate-50 bg-cyan-500 hover:opacity-90 px-4 py-2 mt-4" onClick={() => {setNumber(25)}}>Mudar variável</button>
+			</div>
+		</div>
+  )
+}
+
+export default ManageData
