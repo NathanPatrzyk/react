a454052b1fd76a082722bb7a318519ff807adf1a
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-13 20:59:20 -0300

    CSS e React

---
+++ CSS_e_React/cssEReact/src/App.jsx
@@ -0,0 +1,12 @@
+import MyComponent from "./components/MyComponent";
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl text-red-500 p-4">React com CSS</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ CSS_e_React/cssEReact/src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+const MyComponent = () => {
+  return (
+    <div>
+      <h1 className="p-4">CSS de Componente</h1>
+      <p className="p-2 text-green-500 bg-slate-950">Este é o parágrafo do componente</p>
+      <p className="p-2 text-slate-950 bg-yellow-400">Este tb é do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
