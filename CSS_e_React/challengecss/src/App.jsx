a554575ae49efe1a524f2e7e58cea0664774bd5e
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-17 20:54:01 -0300

    Challenge CSS

--- CSS_e_React/challengecss/src/App.jsx
+++ CSS_e_React/challengecss/src/App.jsx
@@ -1,40 +1,41 @@
 import CarDetails from "./components/CarDetails";
 
 function App() {
+  const cars = [
+    { brand: "Toyota", model: "Corolla", color: "Branco", value: "R$ 160.000" },
+    { brand: "Ford", model: "Mustang", color: "Vermelho", value: "R$ 500.000" },
+    {
+      brand: "Chevrolet",
+      model: "Camaro",
+      color: "Preto",
+      value: "R$ 490.000",
+    },
+    { brand: "Honda", model: "Civic", color: "Prata", value: "R$ 160.000" },
+    { brand: "BMW", model: "M3", color: "Branco", value: "R$ 600.000" },
+    {
+      brand: "Mercedes-Benz",
+      model: "AMG GT Coupé",
+      color: "Preto",
+      value: "R$ 800.000",
+    },
+    { brand: "Audi", model: "R8", color: "Vermelho", value: "R$ 1.200.000" },
+    { brand: "Porsche", model: "911", color: "Azul", value: "R$ 900.000" },
+    { brand: "Nissan", model: "GT-R", color: "Cinza", value: "R$ 1.000.000" },
+  ];
   return (
-    <div className="container mx-auto p-16 text-center">
-      <h1 className="text-red-500 text-3xl font-bold pb-16">Challenge CSS</h1>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <CarDetails
-          brand="Ford"
-          model="Mustang"
-          color="Azul"
-          value="R$ 200.000,00"
-        />
-        <CarDetails
-          brand="Ford"
-          model="Mustang"
-          color="Azul"
-          value="R$ 200.000,00"
-        />
-        <CarDetails
-          brand="Ford"
-          model="Mustang"
-          color="Azul"
-          value="R$ 200.000,00"
-        />
-        <CarDetails
-          brand="Ford"
-          model="Mustang"
-          color="Azul"
-          value="R$ 200.000,00"
-        />
-        <CarDetails
-          brand="Ford"
-          model="Mustang"
-          color="Azul"
-          value="R$ 200.000,00"
-        />
+    <div className="container mx-auto p-8 md:p-16 text-center">
+      <h1 className="text-red-500 text-3xl font-bold pb-8 md:pb-16">
+        Carros Disponíveis
+      </h1>
+      <div className="grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {cars.map((car) => (
+          <CarDetails
+            brand={car.brand}
+            model={car.model}
+            color={car.color}
+            value={car.value}
+          />
+        ))}
       </div>
     </div>
   );
--- CSS_e_React/challengecss/src/components/CarDetails.jsx
+++ CSS_e_React/challengecss/src/components/CarDetails.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const CarDetails = ({ brand, model, color, value }) => {
   return (
-    <div className="bg-slate-900 p-4 mx-auto w-full rounded-md">
-      <figure className="max-w-40 mx-auto rounded-md font-medium">
+    <div className="bg-slate-900 p-2 md:p-4 mx-auto w-full rounded-md border-4 border-transparent hover:border-red-500">
+      <figure className="max-w-40 mx-auto rounded-md font-medium py-2 md:py-4">
         <img className="w-full" src="/car.png" />
       </figure>
-      <div className="flex flex-col gap-1 justify-center pt-4">
-        <p className="text-red-500 text-xl text-center font-bold">{brand}</p>
-        <p className="text-red-500 text-2xl text-center pb-4">{model}</p>
+      <div className="flex flex-col gap-2 md:gap-4 justify-center">
+        <p className="text-red-500 text-xl text-center font-bold pt-1 md:pt-2">{brand}</p>
+        <p className="text-red-500 text-2xl text-center pt-1 md:pb-2">{model}</p>
         <p className="text-red-50 text-center">{color}</p>
-        <p className="text-red-50 text-center pb-4">{value}</p>
+        <p className="text-red-50 text-center">{value}</p>
       </div>
     </div>
   );
