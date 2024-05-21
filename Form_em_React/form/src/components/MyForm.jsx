3d3a955777803cc041519fd93b7a831884e44b30
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-21 18:07:57 -0300

    Trabalhando com forms

--- Form_em_React/form/src/components/MyForm.jsx
+++ Form_em_React/form/src/components/MyForm.jsx
@@ -1,19 +1,47 @@
+import { useState } from "react";
+
 const MyForm = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  console.log(name);
+
   return (
-    <>
-      <form className="flex flex-col p-12 w-96 max-w-[80%] bg-zinc-800 rounded-md">
-        <div className="flex flex-col gap-8">
-          <h2 className="text-2xl text-zinc-50 text-center font-bold border-b-4 border-zinc-50 rounded-sm pb-2 selected:border-none">
-            Login
-          </h2>
-          <div className="flex flex-col gap-1">
-            <label className="text-zinc-600 font-bold mx-2" htmlFor="name">NOME</label>
-            <input className="bg-zinc-600 text-zinc-50 focus-visible:outline-0 border-2 border-transparent focus-visible:border-zinc-500 rounded-md p-2" type="text" name="name" placeholder="Digite o seu nome" />
-          </div>
-          <input className="bg-orange-500 hover:bg-orange-600 text-zinc-50 font-bold rounded-md p-2 px-16 mx-auto" type="submit" value="Enviar" />
-        </div>
-      </form>
-    </>
+    <form className="bg-zinc-800 rounded-md p-8 sm:p-12 w-96 max-w-[80%]">
+      <div className="flex flex-col gap-4">
+        <h2 className="rounded-sm text-2xl text-zinc-50 text-center font-bold border-b-4 border-zinc-50 selected:border-none pb-2 mb-4">
+          Login
+        </h2>
+        <label className="flex flex-col gap-1">
+          <div className="text-zinc-600 font-bold mx-2">NOME</div>
+          <input
+            className="bg-zinc-600 rounded-md text-zinc-50 focus-visible:outline-0 border-2 border-transparent focus-visible:border-zinc-500 p-2"
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+          />
+        </label>
+        <label className="flex flex-col gap-1">
+          <div className="text-zinc-600 font-bold mx-2">E-MAIL</div>
+          <input
+            className="bg-zinc-600 rounded-md text-zinc-50 focus-visible:outline-0 border-2 border-transparent focus-visible:border-zinc-500 p-2"
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+          />
+        </label>
+        <input
+          className="bg-orange-500 hover:bg-orange-600 rounded-md text-zinc-50 font-bold p-2 mx-auto mt-4 w-[160px] max-w-full"
+          type="submit"
+          value="Enviar"
+        />
+      </div>
+    </form>
   );
 };
 
