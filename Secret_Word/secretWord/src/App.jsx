eb3f9dd21fe63980654ee1201ef9e88dfbc6500b
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-06-04 21:25:43 -0300

    Continuando projeto secret word

--- Secret_Word/secretWord/src/App.jsx
+++ Secret_Word/secretWord/src/App.jsx
@@ -1,9 +1,22 @@
+import { wordsList } from "./data/words";
+
+import { useCallback, useEffect, useState } from "react";
+
 import StartScreen from "./components/StartScreen";
 
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
   return (
     <div className="flex justify-center items-center text-center px-8">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <StartScreen />}
+      {gameStage === "end" && <StartScreen />}
     </div>
   );
 }
