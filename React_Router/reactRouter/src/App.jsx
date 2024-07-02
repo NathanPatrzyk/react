24d1f4dc731946b72e91608e73003b478a2ac397
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-07-02 18:16:58 -0300

    Trabalhando com React Router

--- React_Router/reactRouter/src/App.jsx
+++ React_Router/reactRouter/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import SearchForm from "./components/SearchForm";
@@ -8,6 +8,7 @@ import About from "./pages/About";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
 import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -23,6 +24,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products/:id" element={<Product />}></Route>
           <Route path="/products/:id/info" element={<Info />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/company" element={<Navigate to="/about" />} />
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
