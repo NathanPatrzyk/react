73c8528ddb8028bcac32dfd3d1d39b8267406f4b
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-07-06 19:09:14 -0300

    Trabalhando com Context API

--- Context_Api_React/contextApiReact/src/context/TitleColorContext.jsx
+++ Context_Api_React/contextApiReact/src/context/TitleColorContext.jsx
@@ -1,14 +1,27 @@
-import { Children, createContext, useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 export const TitleColorContext = createContext();
 
-export const titleColorReducer = (state, action) => {};
+export const titleColorReducer = (state, action) => {
+  switch (action.type) {
+    case "PURPLE":
+      return { ...state, color: "text-purple-500" };
+    case "RED":
+      return { ...state, color: "text-red-500" };
+    case "BLUE":
+      return { ...state, color: "text-blue-500" };
+    default:
+      return state;
+  }
+};
 
 export const TitleColorContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(titleColorReducer, { color: "orange" });
+  const [state, dispatch] = useReducer(titleColorReducer, {
+    color: "text-orange-500",
+  });
 
   return (
-    <TitleColorContext.Provider value={{ ...state }}>
+    <TitleColorContext.Provider value={{ ...state, dispatch }}>
       {children}
     </TitleColorContext.Provider>
   );
--- Context_Api_React/contextApiReact/src/pages/Home.jsx
+++ Context_Api_React/contextApiReact/src/pages/Home.jsx
@@ -5,14 +5,38 @@ import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 const Home = () => {
   const { counter } = useCounterContext();
-  const { color } = useTitleColorContext();
+  const { color, dispatch } = useTitleColorContext();
+
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  };
 
   return (
     <div>
       <div className="h-screen flex flex-col gap-4 items-center justify-center">
-        <h1 className={`text-${color}-500 text-2xl font-bold`}>Home</h1>
+        <h1 className={`text-2xl font-bold ${color}`}>Home</h1>
         <p className="text-orange-100">Valor do contador: {counter}</p>
         <ChangeCounter />
+        <div className="flex gap-4">
+          <button
+            className="transition ease-in-out duration-300 bg-red-500 hover:bg-red-600  text-neutral-950 rounded-md px-4 py-2"
+            onClick={() => setTitleColor("RED")}
+          >
+            Vermelho
+          </button>
+          <button
+            className="transition ease-in-out duration-300 bg-purple-500 hover:bg-purple-600  text-neutral-950 rounded-md px-4 py-2"
+            onClick={() => setTitleColor("PURPLE")}
+          >
+            Roxo
+          </button>
+          <button
+            className="transition ease-in-out duration-300 bg-blue-500 hover:bg-blue-600  text-neutral-950 rounded-md px-4 py-2"
+            onClick={() => setTitleColor("BLUE")}
+          >
+            Azul
+          </button>
+        </div>
       </div>
     </div>
   );
