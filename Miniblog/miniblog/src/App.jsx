8a227bfb7a35870aa203715740b3713ac9fa77f4
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-07-27 19:51:07 -0300

    Miniblog

--- Miniblog/miniblog/src/App.jsx
+++ Miniblog/miniblog/src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 export function App() {
   return (
     <div className="min-h-[60vh] mb-4">
-      <h1 className="text-5xl text-center pt-4">
-        Mini<span className="font-bold">Blog</span>
-      </h1>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- Miniblog/miniblog/src/firebase/config.js
+++ Miniblog/miniblog/src/firebase/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB0WTTYaROS2-e63ly9rRPLd5YEf6OdAQs",
@@ -10,3 +11,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
