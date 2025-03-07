833dfeb9d30cec8078e9e0a1a18fd40217c88a72
Nathan Patrzyk <nathanpatzyk11@gmail.com>
2025-03-06 21:19:28 -0300

    Miniblog

---
+++ Miniblog/miniblog/src/app.jsx
@@ -0,0 +1,73 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+import { AuthProvider } from "./context/AuthContext";
+
+import { Home } from "./pages/home/home";
+import { About } from "./pages/about/about";
+import { Login } from "./pages/login/login";
+import { Register } from "./pages/register/register";
+import { CreatePost } from "./pages/createPost/createPost";
+import { Dashboard } from "./pages/dashboard/dashboard";
+
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
+
+export function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return (
+      <p className="bg-zinc-900 text-zinc-300 flex justify-center items-center h-screen px-8 md:px-16 py-3">
+        Carregando...
+      </p>
+    );
+  }
+
+  return (
+    <div className="bg-zinc-900 text-zinc-300">
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+
+          <div className="flex items-center justify-center min-h-[90vh]">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              ></Route>
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              ></Route>
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              ></Route>
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              ></Route>
+            </Routes>
+          </div>
+
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+}
